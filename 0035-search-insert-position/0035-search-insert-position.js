54b5df5624fc5b7caad9190d2f44669c04b58f95
Cornel102 <corneliusnkwocha5@gmail.com>
2025-11-11 15:53:35 +0100

    Time: 0 ms (100%), Space: 54.1 MB (66.61%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,10 +4,19 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] >= target) {
-            return i;
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor(left + (right - left) / 2);
+
+        if (nums[mid] === target) {
+            return mid;               
+        } else if (nums[mid] < target) {
+            left = mid + 1;           
+        } else {
+            right = mid - 1;          
         }
     }
-    return nums.length;
+
+    return left;
 };
